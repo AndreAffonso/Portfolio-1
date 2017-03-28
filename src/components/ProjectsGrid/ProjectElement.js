fc6d8f32d86ec48c732337b0f5df6f9ec4d8305b
brunobraga95 <brn95@live.com>
2017-03-28 08:23:23 -0300

    Chapulin internship content not avaiable

--- src/components/ProjectsGrid/ProjectElement.js
+++ src/components/ProjectsGrid/ProjectElement.js
@@ -4,11 +4,12 @@ import { Link } from 'react-router'
 
 class ProjectElement extends React.Component{
   render() {
-    console.log(`/view/${this.props.projectInfo.route}`)
     return (
       <div className='projectElement' style={{backgroundImage: 'url(' + this.props.projectInfo.picture + ')'}}>
-      	<Link to={`/project/${this.props.projectInfo.route}`}>
-          <div className='projectSubtitleWrapper'>
+      	<Link to={this.props.projectInfo.route === 'chapulin-internship'
+         ?`#` : `/project/${this.props.projectInfo.route}`}>
+          <div className={this.props.projectInfo.route === 'chapulin-internship'
+           ? 'projectSubtitleWrapper contentNotAvaiable' : 'projectSubtitleWrapper'}>
             <div className='projectSubtitle'>
           	  <span>{this.props.projectInfo.name}</span>
             </div>
